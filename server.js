db30e31d9edcbf5fb054aa4a8a8aabf5ea3d5491
Muhammad Tifak <moht20009@gmail.com>
2020-06-23 23:57:05 -0700

    Added fancy navbar

--- server.js
+++ server.js
@@ -2,10 +2,18 @@
 const express = require('express')
 const app = express()
 
-// Tell our app to send the "hello world" message to our home page
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// require handlebars
+const exphbs = require('express-handlebars');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+// Render the "home" layout for the main page and send the following msg
+app.get('/', (_, res) => {
+    res.render('layouts/main', {});
+});
 
 // Choose a port to listen on
 const port = process.env.PORT || 3000;
